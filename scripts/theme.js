7130e155195d6992496d0fc39a3adae38f6f8ce2
Ramlakhan Madheshiya <ramlakhanmadheshiya7@gmail.com>
2024-03-08 10:33:03 +0530

    light theme updated

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,13 +1,17 @@
 const themeBtn = document.querySelector('.theme_btn');
-const body = document.body;
-const favicon = document.getElementById('favicon');
+        const body = document.body;
+        const favicon = document.getElementById('favicon');
+        const themeIcon = document.getElementById('theme_icon');
 
-themeBtn.addEventListener('click', () => {
-    body.classList.toggle('light');
-    // Change favicon based on theme
-    if (body.classList.contains('light')) {
-        favicon.href = 'images/light/favicon_light.ico';
-    } else {
-        favicon.href = 'images/dark/favicon_dark.ico';
-    }
-});
+        themeBtn.addEventListener('click', () => {
+            body.classList.toggle('light');
+
+            if (body.classList.contains('light')) {
+                favicon.href = 'images/light/favicon_light.ico';
+                themeIcon.src = 'images/light/theme_light.svg';
+            } else {
+                favicon.href = 'images/dark/favicon_dark.ico';
+                themeIcon.src = 'images/dark/theme_dark.svg';
+            }
+            // function light_theme()
+        });
